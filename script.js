9e5443c6daecb6f5cbf6f0f756bbfc3efc47f202
ec449 <ec449@njit.edu>
2024-11-14 09:01:56 -0500

    11/14 more practice

--- script.js
+++ script.js
@@ -21,6 +21,30 @@ if (userName=="Harry"){
     house="Gryffindor";
     console.log(house);
 }
+let home;
+let number=86;
+
+if (number>90){
+    home="Grade A"
+    console.log(home);
+}
+else if(number>80){
+    home="Grade B"
+    console.log(home);
+
+} 
+else if (number>70){
+    home="Grade 70"
+    console.log(home);
+}
+else{
+    home="fail"
+    console.log(home);
+}
+//else if goes in that order if the first one is right then thats the one that will show
+//EX: 86 is greater than 80and 70 but since 80 is in line 31 and 70 in line 36 the words that show will be grade B
+//so the output of the first correct statment  going in that order
+
 
 
 //order logged is ordered shown 
@@ -36,3 +60,4 @@ console.log(c === d);
 // Is a Comparison Operator that Means Equal to & type
 console.log(e&&f);
 // meaning variable e and f arent both the same, && means And
+console.log(home);
