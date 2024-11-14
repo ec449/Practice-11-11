5a7f579dd1b04f1d38278a427b8d48806d4985d2
ec449 <ec449@njit.edu>
2024-11-14 08:41:56 -0500

    practice 11-11-24

---
+++ script.js
@@ -0,0 +1,38 @@
+var age=20;
+var age=34567890123456788900n;
+//means it will be considered as a bigint number and shows the exact value
+let userName="Harry";
+let FirstName="Erica"
+let LastName="Carangui"
+let pets = true;
+//true or false is a boolean Value
+let house= null;
+//null means intentially left empty
+let a=100;
+let b=25;
+b= b/5;
+
+let c=100;
+let d=25;
+let e=true;
+let f=false;
+
+if (userName=="Harry"){
+    house="Gryffindor";
+    console.log(house);
+}
+
+
+//order logged is ordered shown 
+console.log(age);
+console.log(userName);
+console.log(FirstName + " " + LastName);
+const valuePi = 3.14;
+console.log(valuePi);
+console.log(pets);
+console.log(house);
+console.log (b);
+console.log(c === d);
+// Is a Comparison Operator that Means Equal to & type
+console.log(e&&f);
+// meaning variable e and f arent both the same, && means And
